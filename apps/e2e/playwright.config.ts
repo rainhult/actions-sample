4f42fd8592afa4d24fed901cbdefd86574b0b19e
rainhult <rainhult@gmail.com>
2026-01-29 06:11:28 +0000

    改善

--- apps/e2e/playwright.config.ts
+++ apps/e2e/playwright.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-    testDir: './e2e/tests',
+    testDir: './tests',
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
     workers: process.env.CI ? 1 : undefined,
     reporter: 'html',
+    outputDir: './test-results',
     use: {
         baseURL: 'http://localhost:8080',
         trace: 'on-first-retry',
